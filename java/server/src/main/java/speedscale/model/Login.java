de2e15f45ec2309ae1e8fbc77bbb357b6a338d1b
Ken Ahrens <ken.ahrens@gmail.com>
2023-08-02 15:17:56 -0400

    [SPD-5748] add minor latency bump version (#5)
    * [SPD-5748] add minor latency bump version
    
    * [SPD-5748] ignore

--- java/server/src/main/java/speedscale/model/Login.java
+++ java/server/src/main/java/speedscale/model/Login.java
@@ -1,13 +1,21 @@
 package speedscale.model;
 
+import java.util.Random;
+
 public class Login {
     String username, password;
+    private static final Random r = new Random();
 
     public String getUsername() {
         return username;
     }
 
     public String getPassword() {
+        try {
+            Thread.sleep(r.nextLong(50));
+        } catch (InterruptedException e) {
+            // ignored
+        }
         return password;
     }
 }

fcd92edc05cb1c71b13174a8d63cf0dde786cec2
Kushagra Mansingh <12158241+kushmansingh@users.noreply.github.com>
2026-03-04 09:08:15 -0500

    Use correct date and prevent out of bounds exceptions

--- java/server/src/main/java/speedscale/lib/Treasury.java
+++ java/server/src/main/java/speedscale/lib/Treasury.java
@@ -1,5 +1,6 @@
 package speedscale.lib;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -7,25 +8,26 @@ import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import speedscale.model.TreasuryResponse;
 
 public class Treasury {
 
-    private static final String baseURL = "https://api.fiscaldata.treasury.gov/services/api/fiscal_service";
+  private static final String baseURL =
+      "https://api.fiscaldata.treasury.gov/services/api/fiscal_service";
 
-    private static final HttpClient httpTransport = HttpClient.newHttpClient();
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd");
-    private static ObjectMapper objectMapper = new ObjectMapper();
+  private static final HttpClient httpTransport = HttpClient.newHttpClient();
+  private static final SimpleDateFormat dateFormat =
+      new SimpleDateFormat("yyyy-MM-dd");
+  private static ObjectMapper objectMapper = new ObjectMapper();
 
-    public static TreasuryResponse interestRates(Date d) throws Exception {
-        String date = dateFormat.format(d);
-        HttpRequest req = HttpRequest.newBuilder(
-                URI.create(baseURL + "/v2/accounting/od/avg_interest_rates?filter=record_date:gte:" + date)).GET()
-                .build();
-        HttpResponse<String> resp = httpTransport.send(req, BodyHandlers.ofString());
-        return objectMapper.readValue(resp.body(), TreasuryResponse.class);
-    }
+  public static TreasuryResponse interestRates(Date d) throws Exception {
+    String date = dateFormat.format(d);
+    String url =
+        baseURL +
+        "/v2/accounting/od/avg_interest_rates?filter=record_date:gte:" + date;
+    HttpRequest req = HttpRequest.newBuilder(URI.create(url)).GET().build();
+    HttpResponse<String> resp =
+        httpTransport.send(req, BodyHandlers.ofString());
+    return objectMapper.readValue(resp.body(), TreasuryResponse.class);
+  }
 }
